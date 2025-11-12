edc386207662e4c9fc2a49e9973253db73916d00
Shrijit Vivek <shrijitv3@gmail.com>
2025-11-12 17:10:28 +0530

    target no sum from array

--- two sums.js
+++ two sums.js
@@ -21,3 +21,18 @@
 // Output: [0,1]
 
 
+var twoSum = function(nums, target) {
+    let result = []
+    for(let i = 0; i<=nums.length-1; i++){
+        for(let j = i+1; j<nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                result.push(i,j)
+                return result
+            }
+        }
+    }
+    
+};
+let nums = [1,2,3,4]
+let target = 7
+twoSum(nums,target)
