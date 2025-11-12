87d5b45440bc469f213c5704a8be313a85214a73
Shrijit Vivek <shrijitv3@gmail.com>
2025-11-12 17:13:24 +0530

    sorted array elements squares

---
+++ squares.js
@@ -0,0 +1,42 @@
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-4,-1,0,3,10]
+// Output: [0,1,9,16,100]
+// Explanation: After squaring, the array becomes [16,1,0,9,100].
+// After sorting, it becomes [0,1,9,16,100].
+// Example 2:
+
+// Input: nums = [-7,-3,2,3,11]
+// Output: [4,9,9,49,121]
+ 
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let left = 0
+    let right = nums.length - 1
+    let result = []
+    let index = nums.length - 1
+   
+   while(left <= right){
+    let leftSquare = nums[left] * nums[left] 
+    let rightSquare = nums[right] * nums[right]
+
+    if(leftSquare < rightSquare){
+    result[index] = rightSquare
+    right--
+    }
+    else{
+        result[index] = leftSquare
+        left++
+    }
+    index--
+   }
+   return result
+    
+};
